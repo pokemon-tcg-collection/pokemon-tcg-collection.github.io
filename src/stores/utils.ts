0e8ab796362dcf31509577e4d58a76c5f04b4711
pokemon-tcg-collection <querela92+pokemon-tcg-collection@gmail.com>
2025-10-16 16:24:18 +0200

    Fix date handling in toRawDeep

--- src/stores/utils.ts
+++ src/stores/utils.ts
@@ -4,15 +4,14 @@ import { toRaw } from 'vue'
 export function toRawDeep<T>(observed: T): T {
   const val = toRaw(observed)
 
-  if (Array.isArray(val)) {
-    return val.map(toRawDeep) as T
-  }
-
+  if (Array.isArray(val)) return val.map(toRawDeep) as T
   if (val === null) return null as T
+
+  // NOTE: do not try to recreate inbuilt objects
+  if (val instanceof Date) return val
 
   if (typeof val === 'object') {
     const entries = Object.entries(val).map(([key, val]) => [key, toRawDeep(val)])
-
     return Object.fromEntries(entries)
   }
 

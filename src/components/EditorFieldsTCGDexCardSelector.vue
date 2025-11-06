18a83504a865630f87c77bd3ed2e4d7f4ab9c385
pokemon-tcg-collection <querela92+pokemon-tcg-collection@gmail.com>
2025-11-06 23:40:32 +0100

    Fancies TCGDex API autocomplete

--- src/components/EditorFieldsTCGDexCardSelector.vue
+++ src/components/EditorFieldsTCGDexCardSelector.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
-import type { CardResume, Set, SetResume, SupportedLanguages, Card as TCGCard } from '@tcgdex/sdk'
+import type {
+  CardResume,
+  SerieModel,
+  Set,
+  SetResume,
+  SupportedLanguages,
+  Card as TCGCard,
+} from '@tcgdex/sdk'
 import TCGdex, { CardResumeModel, Query, SetResumeModel } from '@tcgdex/sdk'
 import { computed, readonly, ref, watch } from 'vue'
 
 import { CARD_LANGUAGES, TCGDEX_LANGUAGES } from '@/model/interfaces'
+import { highlightAutocompleteItem } from '@/utils/autocomplete'
+
+interface SetItem {
+  id: string
+  label: string
+  image?: string
+  set?: SetResume
+}
 
 const emit = defineEmits<{ cardSelected: [card: TCGCard] }>()
 
 const languages = readonly(CARD_LANGUAGES)
-const sets = ref<{ id: string; label: string; image?: string; set?: SetResume }[]>([])
-const cards = ref<{ id: string; label: string; image?: string; card?: CardResume }[]>([])
+const sets = ref<(SetItem | { type: 'subheader'; title: string })[]>([])
+const cards = ref<{ id: string; nr: string; label: string; image?: string; card?: CardResume }[]>(
+  [],
+)
 const boosters = ref<{ id: string; label: string }[]>([])
 
 // TODO: maybe use settings for default
@@ -20,6 +37,9 @@ const tcgdex_id = ref<string>()
 
 const isLoadingSets = ref(false)
 const isLoadingCards = ref(false)
+
+const searchSet = ref<string>('')
+const searchCard = ref<string>('')
 
 const tcgdex = computed(() => {
   const tcgdex_language = TCGDEX_LANGUAGES.includes(language.value as unknown as SupportedLanguages)
@@ -41,13 +61,29 @@ async function updateTCGData() {
 
   isLoadingSets.value = true
 
-  const tcgSets = await tcgdex.value.set.list()
-  sets.value = tcgSets.map((set) => ({
-    id: set.id,
-    label: set.name,
-    image: set.symbol ? `${set.symbol}.png` : undefined,
-    set: set,
-  }))
+  const tcgSeriesListPreview = await tcgdex.value.serie.list()
+  const tcgSeriesList = (await Promise.all(
+    tcgSeriesListPreview.map(async (series) => await series.getSerie()),
+  )) as SerieModel[]
+  Object.assign(window, { tcgSeriesList })
+
+  sets.value = tcgSeriesList
+    .map((series) =>
+      (
+        [{ type: 'subheader', title: series.name, series: series }] as (
+          | SetItem
+          | { type: 'subheader'; title: string; series: SerieModel }
+        )[]
+      ).concat(
+        series.sets.map((set) => ({
+          id: set.id,
+          label: set.name,
+          image: set.symbol ? `${set.symbol}.png` : undefined,
+          set: set,
+        })),
+      ),
+    )
+    .flat(1)
 
   isLoadingSets.value = false
 }
@@ -73,8 +109,9 @@ async function updateTCGCardData() {
   const tcgCards = await tcgdex.value.card.list(Query.create().equal('set', newSetId))
   cards.value = tcgCards.map((setCard) => ({
     id: setCard.id,
+    nr: setCard.localId,
     label: setCard.name,
-    // image: setCard.image ? setCard.getImageURL('low', 'png') : undefined,
+    image: setCard.image ? setCard.getImageURL('low', 'png') : undefined,
     card: setCard,
   }))
 
@@ -90,7 +127,9 @@ async function onSetSelected(setId: string) {
   let tcgSet: Set | null | undefined = undefined
 
   if (sets.value.length > 0) {
-    const tcgSetInfo = sets.value.find((set) => set.id === setId)?.set
+    const tcgSetInfo = (sets.value.filter((set) => !Object.hasOwn(set, 'type')) as SetItem[]).find(
+      (set) => set.id === setId,
+    )?.set
     if (tcgSetInfo !== undefined && tcgSetInfo instanceof SetResumeModel) {
       tcgSet = await tcgSetInfo.getSet()
     }
@@ -164,6 +203,7 @@ watch(
   <v-autocomplete
     v-model="set"
     @update:model-value="onSetSelected"
+    @update:search="(val) => (searchSet = val)"
     :items="sets"
     item-value="id"
     item-title="label"
@@ -172,11 +212,51 @@ watch(
     clearable
     hide-no-data
   >
+    <template #subheader="{ index, props }">
+      <v-divider v-if="index > 0"></v-divider>
+      <v-list-item disabled :style="{ opacity: 1 }">
+        <template #prepend>
+          <v-avatar rounded="0" :style="{ width: '100px', height: '60px' }">
+            <v-img :src="`${(props.series as SerieModel).logo}.png`" :cover="false"></v-img>
+          </v-avatar>
+        </template>
+        <template #title>
+          <v-list-item-title :style="{ opacity: 0.6 }">{{
+            props.title as string
+          }}</v-list-item-title>
+        </template>
+        <template #subtitle>
+          <v-list-item-subtitle>{{
+            (props.series as { releaseDate: string }).releaseDate
+          }}</v-list-item-subtitle>
+        </template>
+      </v-list-item>
+    </template>
     <template #item="{ item, props }">
       <v-list-item v-bind="props">
         <template #prepend>
-          <v-avatar :image="item.raw.image"></v-avatar>
+          <v-avatar rounded="0">
+            <v-img :src="(item.raw as SetItem).image" :cover="false"></v-img>
+          </v-avatar>
         </template>
+        <template #title
+          ><component :is="() => highlightAutocompleteItem(item, searchSet)"
+        /></template>
+        <template #subtitle v-if="(item.raw as SetItem).set"
+          >{{ (item.raw as SetItem).set!.cardCount.official
+          }}<template
+            v-if="
+              (item.raw as SetItem).set!.cardCount.official !==
+              (item.raw as SetItem).set!.cardCount.total
+            "
+          >
+            (+{{
+              (item.raw as SetItem).set!.cardCount.total -
+              (item.raw as SetItem).set!.cardCount.official
+            }})</template
+          >
+          Cards</template
+        >
       </v-list-item>
     </template>
   </v-autocomplete>
@@ -184,6 +264,7 @@ watch(
   <v-autocomplete
     v-model="tcgdex_id"
     @update:model-value="onCardSelected"
+    @update:search="(val) => (searchCard = val)"
     :items="cards"
     item-value="id"
     item-title="label"
@@ -191,7 +272,20 @@ watch(
     label="Card"
     clearable
     hide-no-data
-  ></v-autocomplete>
+    :menu="true"
+  >
+    <template #item="{ item, props }">
+      <v-list-item v-bind="props">
+        <template #prepend>
+          <v-avatar :image="item.raw.image" rounded="0"></v-avatar>
+          <span class="ms-4 text-grey-lighten-1">#{{ item.raw.nr }}</span>
+        </template>
+        <template #title
+          ><component :is="() => highlightAutocompleteItem(item, searchCard)"
+        /></template>
+      </v-list-item>
+    </template>
+  </v-autocomplete>
 
   <v-autocomplete
     v-model="booster"
